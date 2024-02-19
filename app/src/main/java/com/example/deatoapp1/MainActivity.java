86ca1114cd4b2690eccd986acc050e25c4845371
Sophia <sdeato011@gmail.com>
2024-02-19 20:33:05 +0800

    Codes

---
+++ app/src/main/java/com/example/deatoapp1/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.deatoapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView name;
+    TextView myName;
+    TextView email;
+    TextView myEmail;
+    TextView contact;
+    TextView myContact;
+    TextView myDisplay;
+    Button myButton;
+    Button myClear;
+    EditText myInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //Constructors
+        name = (TextView) findViewById(R.id.txtName);
+        myName = (TextView) findViewById(R.id.txtMyName);
+        email = (TextView) findViewById(R.id.txtEmail);
+        myEmail = (TextView) findViewById(R.id.txtMyEmail);
+        contact = (TextView) findViewById(R.id.txtContact);
+        myContact = (TextView) findViewById(R.id.txtMyContact);
+        myDisplay = (TextView) findViewById(R.id.txtDisplay);
+        myButton = (Button) findViewById(R.id.btnClick);
+        myClear = (Button) findViewById(R.id.btnClear);
+
+        myInput = (EditText) findViewById(R.id.txtName2);
+
+
+
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myValue = myInput.getText().toString();
+                myDisplay.setText("Hello \n" + myValue +"!");
+            }
+        });
+
+
+
+        myClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDisplay.setText("empty...");
+            }
+        });
+    }
+}
